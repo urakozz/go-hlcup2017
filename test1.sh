357f79c3d68e7a79b712145afcc3f6ef782b34a9
Yury Kozyrev <y.kozyrev@travelaudience.com>
2017-09-04 17:59:31 +0200

    rm test

--- test1.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-curl localhost:3000/users/new -XPOST -d '{"id":10,"email":"ff@ff.ru","first_name":"Елизавета","last_name":"Хопленлова","gender":"f","birth_date":406080000}'
-curl localhost:3000/locations/new -XPOST -d '{"id":100,"distance":9,"city":"омск","place":"Рест","country":"элла"}'
-curl localhost:3000/locations/new -XPOST -d '{"id":101,"distance":9,"city":"о1","place":"Рест11","country":"элла1"}'
-curl localhost:3000/visits/new -XPOST -d '{"id": 1000, "user": 10, "visited_at": 1385572234, "location": 100, "mark": 2}'
-curl localhost:3000/visits/1000 -XPOST -d '{"location": 101}'
-curl localhost:3000/users/10/visits
